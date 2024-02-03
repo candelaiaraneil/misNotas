8df06f39f9caa791a69244b299177bcb8d43ffd7
Candela <caianeil27@gmail.com>
2024-02-02 22:53:56 -0300

    validaciones de pantalla 'crearCuenta'

--- app/src/main/java/com/example/misnotas/crearCuenta.java
+++ app/src/main/java/com/example/misnotas/crearCuenta.java
@@ -3,6 +3,7 @@ package com.example.misnotas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -29,5 +30,35 @@ public class crearCuenta extends AppCompatActivity {
         loginBotonTextView = findViewById(R.id.loginTextView_btn);
 
         //configuración de detector de clics y validación de datos
+
+        crearCuentaBoton.setOnClickListener(v-> crearUnaCuenta());
+        loginBotonTextView.setOnClickListener(v-> finish());
+
+    }
+
+    //lógicas para crear cuenta - recepción de datos
+    void crearUnaCuenta(){
+        String email = emailEditText.getText().toString();
+        String contrasena = contrasenaEditText.getText().toString();
+        String confirmarContrasena = confirmarContrasenaeditText.getText().toString();
+
+        boolean estaValidado = validarDatos(email, contrasena, confirmarContrasena);
+    }
+
+    //creación de método para validar datos ingresados por el usuario - booleano
+    boolean validarDatos(String email, String contrasena, String confirmarContrasena){
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("El email ingresado es inválido");
+            return false;
+        }
+        if (contrasena.length()<6){
+            contrasenaEditText.setError("Tu contraseña tiene pocos caracteres");
+            return false;
+        }
+        if (!contrasena.equals(confirmarContrasena)){
+            confirmarContrasenaeditText.setError("La contraseña ingresada no coincide");
+            return false;
+        }
+        return true;
     }
 }
