133c765bd5b09287f00bb3b05f508eafc3fa9b38
Candela <caianeil27@gmail.com>
2024-02-11 18:30:12 -0300

    se implementa funcionalidad de autenticación Firebase

--- app/src/main/java/com/example/misnotas/crearCuenta.java
+++ app/src/main/java/com/example/misnotas/crearCuenta.java
@@ -1,13 +1,21 @@
 package com.example.misnotas;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class crearCuenta extends AppCompatActivity {
 
@@ -43,6 +51,46 @@ public class crearCuenta extends AppCompatActivity {
         String confirmarContrasena = confirmarContrasenaeditText.getText().toString();
 
         boolean estaValidado = validarDatos(email, contrasena, confirmarContrasena);
+        if(!estaValidado){
+            return;
+        }
+        crearCuentaEnFirebase(email, contrasena);
+    }
+
+    //creación de método para crear cuenta en Firebase - crea la cuenta, envía email de confirmación y cierra sesión para poder iniciar nuevamente
+    void crearCuentaEnFirebase(String email, String contrasena){
+        cambioEnProgreso(true);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuth.createUserWithEmailAndPassword(email, contrasena).addOnCompleteListener(crearCuenta.this,
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        cambioEnProgreso(false);
+                        if (task.isSuccessful()){
+                            //se creó la cuenta correctamente
+                            Toast.makeText(crearCuenta.this, "La cuenta se ha creado correctamente, revisá tu e-mail para corroborarlo",Toast.LENGTH_SHORT).show();
+                            firebaseAuth.getCurrentUser().sendEmailVerification();
+                            firebaseAuth.signOut();
+                            finish();
+                        }else {
+                            //la creación de cuenta falló
+                            Toast.makeText(crearCuenta.this,task.getException().getLocalizedMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        );
+    }
+
+    //creación de método para espera de creación de cuenta en Firebase
+    void cambioEnProgreso(boolean enProgreso){
+        if(enProgreso){
+            progressBar.setVisibility(View.VISIBLE);
+            crearCuentaBoton.setVisibility(View.GONE);
+        }else {
+            progressBar.setVisibility(View.GONE);
+            crearCuentaBoton.setVisibility(View.VISIBLE);
+        }
     }
 
     //creación de método para validar datos ingresados por el usuario - booleano
