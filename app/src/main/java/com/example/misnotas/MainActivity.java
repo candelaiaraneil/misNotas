08bda4e3443fa2b47189f1371599a9997be6ca4b
Candela <caianeil27@gmail.com>
2024-02-14 17:17:38 -0300

    se crea 'detallesNota' para comenzar a trabajar en CRUD notas en la app

--- app/src/main/java/com/example/misnotas/MainActivity.java
+++ app/src/main/java/com/example/misnotas/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.misnotas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton anadirNotaBoton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        anadirNotaBoton = findViewById(R.id.anadir_nota_boton);
+
+        anadirNotaBoton.setOnClickListener((v)-> startActivity(new Intent(MainActivity.this, detallesNota.class)));
     }
 }
---
+++ app/src/main/java/com/example/misnotas/detallesNota.java
@@ -0,0 +1,14 @@
+package com.example.misnotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class detallesNota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles_nota);
+    }
+}
