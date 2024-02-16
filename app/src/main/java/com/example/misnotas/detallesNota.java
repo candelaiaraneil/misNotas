1023aa6189b6719e788e93cdfd71d2aee30dcfa0
Candela <caianeil27@gmail.com>
2024-02-16 13:49:45 -0300

    se termina diseño de 'añadirNota'

--- app/src/main/java/com/example/misnotas/detallesNota.java
+++ app/src/main/java/com/example/misnotas/detallesNota.java
@@ -3,12 +3,35 @@ package com.example.misnotas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class detallesNota extends AppCompatActivity {
+
+    EditText tituloEditText, contenidoEditText;
+    ImageButton guardarNotaBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalles_nota);
+
+        tituloEditText = findViewById(R.id.tituloNotas_texto);
+        contenidoEditText = findViewById(R.id.contenidoNotas_texto);
+        guardarNotaBtn = findViewById(R.id.guardarNota_btn);
+
+        guardarNotaBtn.setOnClickListener((v)-> guardarNota());
+
+
+    }
+
+    void guardarNota(){
+        String notaTitulo = tituloEditText.getText().toString();
+        String notaContenido = contenidoEditText.getText().toString();
+        //validaciones
+        if(notaTitulo==null || notaTitulo.isEmpty()) {
+            tituloEditText.setError("El título es necesario");
+            return;
+        }
     }
 }
